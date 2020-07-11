6cbc1339ffe2e645d667ea3c000bc706775a93df
sosukesuzuki <aosukeke@gmail.com>
2020-07-12 00:49:39 +0900

    Refactor

---
+++ src/category.js
@@ -0,0 +1,53 @@
+/**
+ * @type {(Array<{ name: string; ext: string }>)}
+ */
+const categories = [
+  {
+    name: "angular",
+    ext: "",
+  },
+  {
+    name: "handlebars",
+    ext: "",
+  },
+  {
+    name: "api",
+    ext: "js",
+  },
+  {
+    name: "lwc",
+    ext: "",
+  },
+  {
+    name: "javascript",
+    ext: "js",
+  },
+  {
+    name: "cli",
+    ext: "sh",
+  },
+  {
+    name: "flow",
+    ext: "js",
+  },
+  {
+    name: "graphql",
+    ext: "gql",
+  },
+  {
+    name: "json",
+    ext: "json",
+  },
+  {
+    name: "markdown",
+    ext: "md",
+  },
+  {
+    name: "typescript",
+    ext: "ts",
+  },
+];
+
+module.exports = {
+  categories,
+};
--- src/codeblock.js
+++ src/codeblock.js
@@ -1,32 +1,12 @@
+const { categories } = require("./category");
+
 /**
  * Get file ext for markdown code block from category name.
  * @param {String} category
  * @returns {String}
  */
 function getCodeBlockFileExt(category) {
-  switch (category) {
-    case "angular":
-    case "handlebars":
-    case "lwc":
-      return "";
-    case "api":
-    case "javascript":
-      return "js";
-    case "cli":
-      return "sh";
-    case "flow":
-      return "js";
-    case "graphql":
-      return "gql";
-    case "json":
-      return "json";
-    case "markdown":
-      return "md";
-    case "typescript":
-      return "ts";
-    default:
-      return "";
-  }
+  return categories.find(({ name }) => name === category).ext;
 }
 
 /**
